c6858c0f8c69e399cb3442a6845d2995d903eda5
Haseeb Ullah <haseebabbasi00@gmail.com>
2021-02-01 01:28:27 +0500

    Picture App

---
+++ P_I_C_T_U_R_E  A_P_P/P_I_C_T_U_R_E  A_P_P/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace P_I_C_T_U_R_E__A_P_P
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button4 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button5 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button6 = new Guna.UI2.WinForms.Guna2Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2ComboBox1
+            // 
+            this.guna2ComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.guna2ComboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.guna2ComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.guna2ComboBox1.HoverState.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.ItemHeight = 30;
+            this.guna2ComboBox1.ItemsAppearance.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Location = new System.Drawing.Point(23, 43);
+            this.guna2ComboBox1.Name = "guna2ComboBox1";
+            this.guna2ComboBox1.ShadowDecoration.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Size = new System.Drawing.Size(195, 36);
+            this.guna2ComboBox1.TabIndex = 0;
+            this.guna2ComboBox1.SelectedIndexChanged += new System.EventHandler(this.guna2ComboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(23, 127);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(195, 355);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Location = new System.Drawing.Point(224, 43);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(404, 439);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 2;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(634, 43);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(140, 46);
+            this.guna2Button1.TabIndex = 3;
+            this.guna2Button1.Text = "Next";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Location = new System.Drawing.Point(634, 118);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(140, 46);
+            this.guna2Button2.TabIndex = 4;
+            this.guna2Button2.Text = "Previous";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button2_Click);
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.CheckedState.Parent = this.guna2Button3;
+            this.guna2Button3.CustomImages.Parent = this.guna2Button3;
+            this.guna2Button3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button3.ForeColor = System.Drawing.Color.White;
+            this.guna2Button3.HoverState.Parent = this.guna2Button3;
+            this.guna2Button3.Location = new System.Drawing.Point(634, 200);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.ShadowDecoration.Parent = this.guna2Button3;
+            this.guna2Button3.Size = new System.Drawing.Size(140, 46);
+            this.guna2Button3.TabIndex = 5;
+            this.guna2Button3.Text = "Play";
+            this.guna2Button3.Click += new System.EventHandler(this.guna2Button3_Click);
+            // 
+            // guna2Button4
+            // 
+            this.guna2Button4.CheckedState.Parent = this.guna2Button4;
+            this.guna2Button4.CustomImages.Parent = this.guna2Button4;
+            this.guna2Button4.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button4.ForeColor = System.Drawing.Color.White;
+            this.guna2Button4.HoverState.Parent = this.guna2Button4;
+            this.guna2Button4.Location = new System.Drawing.Point(634, 280);
+            this.guna2Button4.Name = "guna2Button4";
+            this.guna2Button4.ShadowDecoration.Parent = this.guna2Button4;
+            this.guna2Button4.Size = new System.Drawing.Size(140, 46);
+            this.guna2Button4.TabIndex = 6;
+            this.guna2Button4.Text = "Pause";
+            this.guna2Button4.Click += new System.EventHandler(this.guna2Button4_Click);
+            // 
+            // guna2Button5
+            // 
+            this.guna2Button5.CheckedState.Parent = this.guna2Button5;
+            this.guna2Button5.CustomImages.Parent = this.guna2Button5;
+            this.guna2Button5.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button5.ForeColor = System.Drawing.Color.White;
+            this.guna2Button5.HoverState.Parent = this.guna2Button5;
+            this.guna2Button5.Location = new System.Drawing.Point(634, 360);
+            this.guna2Button5.Name = "guna2Button5";
+            this.guna2Button5.ShadowDecoration.Parent = this.guna2Button5;
+            this.guna2Button5.Size = new System.Drawing.Size(140, 46);
+            this.guna2Button5.TabIndex = 7;
+            this.guna2Button5.Text = "First";
+            this.guna2Button5.Click += new System.EventHandler(this.guna2Button5_Click);
+            // 
+            // guna2Button6
+            // 
+            this.guna2Button6.CheckedState.Parent = this.guna2Button6;
+            this.guna2Button6.CustomImages.Parent = this.guna2Button6;
+            this.guna2Button6.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button6.ForeColor = System.Drawing.Color.White;
+            this.guna2Button6.HoverState.Parent = this.guna2Button6;
+            this.guna2Button6.Location = new System.Drawing.Point(634, 438);
+            this.guna2Button6.Name = "guna2Button6";
+            this.guna2Button6.ShadowDecoration.Parent = this.guna2Button6;
+            this.guna2Button6.Size = new System.Drawing.Size(140, 46);
+            this.guna2Button6.TabIndex = 8;
+            this.guna2Button6.Text = "Last";
+            this.guna2Button6.Click += new System.EventHandler(this.guna2Button6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(329, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 515);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2Button6);
+            this.Controls.Add(this.guna2Button5);
+            this.Controls.Add(this.guna2Button4);
+            this.Controls.Add(this.guna2Button3);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.guna2ComboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button4;
+        private Guna.UI2.WinForms.Guna2Button guna2Button5;
+        private Guna.UI2.WinForms.Guna2Button guna2Button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ P_I_C_T_U_R_E  A_P_P/P_I_C_T_U_R_E  A_P_P/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+namespace P_I_C_T_U_R_E__A_P_P
+{
+    
+    public partial class Form1 : Form
+    {
+
+        DriveInfo[] Drive;
+        DirectoryInfo[] Directories;
+        String[] Path1;
+        DirectoryInfo Dir;
+        int count = 0;
+        int count1 = 0;
+        bool flag = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Drive = DriveInfo.GetDrives();
+            foreach(DriveInfo Dri in Drive)
+            {
+                if(Dri.IsReady)
+                {
+                    guna2ComboBox1.Items.Add(Dri);
+                }
+            }
+        }
+
+        private void guna2ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            DirectoryInfo Dir = new DirectoryInfo(guna2ComboBox1.SelectedItem.ToString());
+            Directories = Dir.GetDirectories();
+            foreach(DirectoryInfo D in Directories)
+            {
+                listBox1.Items.Add(D);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            count = 0;
+            String[] Path = Directory.GetFiles(guna2ComboBox1.SelectedItem.ToString()+listBox1.SelectedItem.ToString());
+            String[] Ext;
+            foreach (String P in Path)
+            {
+                Ext=Path[count].Split('.');
+                if (Ext[1].Equals("PNG") || Ext[1].Equals("JPG") || Ext[1].Equals("GIF") || Ext[1].Equals("png") || Ext[1].Equals("jpg") || Ext[1].Equals("gif"))
+                {
+                    count1++;
+                }
+                count++;
+            }
+            
+            Path1 = new String[count1];
+            count = 0;
+            int i = 0;
+            foreach(String P in Path)
+            {
+                Ext = Path[count].Split('.');
+                if (Ext[1].Equals("PNG") || Ext[1].Equals("JPG") || Ext[1].Equals("GIF") || Ext[1].Equals("png") || Ext[1].Equals("jpg") || Ext[1].Equals("gif"))
+                {
+                    Path1[i] = Path[count];
+                    i++;
+                }
+                count++;
+            }
+            i = 0;
+            count = 1;
+            if (count1 != 0)
+            {
+                guna2PictureBox1.ImageLocation = Path1[i];
+                label1.Text = "";
+            }
+            else
+                label1.Text = "There is no Image in this folder";
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            
+            if(count==count1-1&&count1!=0)
+            {
+                
+                guna2PictureBox1.ImageLocation = Path1[count];
+                count = 0;
+                label1.Text = "";
+                count++;
+            }
+            else if(count<count1&&count1!=0)
+            {
+                guna2PictureBox1.ImageLocation = Path1[count];
+                label1.Text = "";
+                count++;
+            }
+            if(count1==0)
+                label1.Text = "There is no Image in this folder";
+            flag = true;
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            if (count == 0&&count1!=0)
+            {
+                guna2PictureBox1.ImageLocation = Path1[count];
+                count--;
+                label1.Text = "";
+                count = count1-1;
+                
+            }
+            else if (count <= count1&&count1!=0)
+            {
+                guna2PictureBox1.ImageLocation = Path1[count];
+                count--;
+                label1.Text = "";
+            }
+            if (count1 == 0)
+                label1.Text = "There is no Image in this folder";
+            flag = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(flag==true)
+            {
+                guna2Button1_Click(sender, e);
+            }
+            else
+            {
+                guna2Button2_Click(sender, e);
+            }
+        }
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void guna2Button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void guna2Button5_Click(object sender, EventArgs e)
+        {
+            guna2PictureBox1.ImageLocation = Path1[0];
+        }
+
+        private void guna2Button6_Click(object sender, EventArgs e)
+        {
+            guna2PictureBox1.ImageLocation = Path1[count1-1];
+        }
+    }
+}
